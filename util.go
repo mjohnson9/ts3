dc63c32a97cd89533fbf5972adfc0a186f65195f
NightExcessive <nightexcessive@gmail.com>
2012-09-24 10:44:26 -0400

    Made escape function use a map

--- util.go
+++ util.go
@@ -4,21 +4,26 @@ import (
 	"strings"
 )
 
+var escapeStrings = map[string]string{
+	"\\": "\\\\",
+	" ":  "\\s",
+	"|":  "\\p",
+	"\a": "\\a",
+	"\b": "\\b",
+	"\f": "\\f",
+	"\n": "\\n",
+	"\r": "\\r",
+	"\t": "\\t",
+	"\v": "\\v",
+}
+
 // Escapes a string as specified in the TeamSpeak 3 ServerQuery manual
 func EscapeTS3String(str string) (returnString string) {
 	returnString = str
 
-	returnString = strings.Replace(returnString, "\\", "\\\\", -1)
-	returnString = strings.Replace(returnString, "/", "\\/", -1)
-	returnString = strings.Replace(returnString, " ", "\\s", -1)
-	returnString = strings.Replace(returnString, "|", "\\p", -1)
-	returnString = strings.Replace(returnString, "\a", "\\a", -1)
-	returnString = strings.Replace(returnString, "\b", "\\b", -1)
-	returnString = strings.Replace(returnString, "\f", "\\f", -1)
-	returnString = strings.Replace(returnString, "\n", "\\n", -1)
-	returnString = strings.Replace(returnString, "\r", "\\r", -1)
-	returnString = strings.Replace(returnString, "\t", "\\t", -1)
-	returnString = strings.Replace(returnString, "\v", "\\v", -1)
+	for oldString, newString := range escapeStrings {
+		returnString = strings.Replace(returnString, oldString, newString, -1)
+	}
 
 	return
 }
